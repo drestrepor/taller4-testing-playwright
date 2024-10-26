7683b35c9641ec207117d6ad9580cd708fa2279d
drestrepor <drestrepor@eafit.edu.co>
2024-10-26 17:45:19 -0500

    Fix tests

--- tests/todolist.spec.ts
+++ tests/todolist.spec.ts
@@ -17,38 +17,17 @@ test.describe("Suite de pruebas", () => {
       });
 
     test('Complete Task', async ({ page }) => {
-        await page.getByPlaceholder('What needs to be done?').click();
-        await page.getByPlaceholder('What needs to be done?').fill('Hacer tarea');
-        await page.getByPlaceholder('What needs to be done?').press('Enter');
-        await expect(page.getByText('Hacer tarea')).toBeVisible();
-        await page.getByRole('navigation').getByRole('link', { name: 'Login' }).click();
-        await page.getByText('Get a test account').click();
-        await page.locator('#login-btn').click();
-        await page.locator('span').filter({ hasText: 'check_box_outline_blank Drive' }).locator('i').click();
-        await expect(page.locator('span').filter({ hasText: 'check_box Drive a motorcycle' }).locator('i')).toBeVisible();
-    });
-
-    test('Clear Task', async ({ page }) => {
-        await page.getByPlaceholder('What needs to be done?').click();
-        await page.getByPlaceholder('What needs to be done?').fill('Hacer tarea');
-        await page.getByPlaceholder('What needs to be done?').press('Enter');
-        await expect(page.getByText('Hacer tarea')).toBeVisible();
-        await page.getByRole('navigation').getByRole('link', { name: 'Login' }).click();
-        await page.getByText('Get a test account').click();
-        await page.locator('#login-btn').click();
-        await page.locator('span').filter({ hasText: 'check_box_outline_blank Drive' }).locator('i').click();
-        await expect(page.locator('span').filter({ hasText: 'check_box Drive a motorcycle' }).locator('i')).toBeVisible();
-        await page.getByRole('navigation').getByRole('link', { name: 'Login' }).click();
-        await page.getByText('Get a test account').click();
-        await page.locator('#login-btn').click();
-        await page.getByText('clear_allClear').click();
-        await page.getByText('clear_allClear').click();
         await page.getByRole('navigation').getByRole('link', { name: 'Login' }).click();
         await page.getByText('Get a test account').click();
         await page.locator('#login-btn').click();
         await page.locator('span').filter({ hasText: 'check_box_outline_blank Witness something truly majestic' }).locator('i').click();
-        await page.locator('span').filter({ hasText: 'check_box_outline_blank Help' }).locator('i').click();
-        await page.getByText('check_box_outline_blank').click();
+    });
+
+    test('Clear Task', async ({ page }) => {
+        await page.getByRole('navigation').getByRole('link', { name: 'Login' }).click();
+        await page.getByText('Get a test account').click();
+        await page.locator('#login-btn').click();
+        await page.locator('span').filter({ hasText: 'check_box_outline_blank Witness something truly majestic' }).locator('i').click();
         await page.getByText('clear_allClear').click();
     });
 
